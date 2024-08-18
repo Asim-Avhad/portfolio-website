7c4f271fd7808908de435f27b8d5f5e75ce31c99
Asim Avhad <avhadasim30@gmail.com>
2024-08-18 20:38:11 +0530

    modified About.tsx, Intro.tsx added SectionDivider for mobile devices

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -115,7 +115,7 @@ export default function Intro({}: Props) {
           </a>
 
           <a
-            className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+            className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack transform origin-center dark:bg-white/10 dark:text-white/60 "
             href="https://github.com/asim-avhad"
             target="_blank"
           >
--- components/SectionDivider.tsx
+++ components/SectionDivider.tsx
@@ -8,7 +8,7 @@ type Props = {}
 export default function SectionDivider({}: Props) {
   return (
     <motion.div
-    className="bg-gray-200 my-24 h-16 w-1 rounded-full hidden sm:block dark:bg-opacity-20"
+    className="bg-gray-200 my-24 h-12 w-0.5 rounded-full sm:h-16 sm:w-1 dark:bg-opacity-20"
     initial={{ opacity: 0, y: 100 }}
     animate={{ opacity: 1, y: 0 }}
     transition={{ delay: 0.125 }}
