f54907b9cce700b6033e824477a357fca857545c
Asim Avhad <avhadasim30@gmail.com>
2024-08-19 08:49:11 +0530

    Modification of timeline element-Experience.tsx

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -11,55 +11,71 @@ import { experiencesData } from "@/lib/data";
 import { useSectionInView } from "@/lib/hooks";
 import { useTheme } from "@/context/ThemeContext";
 import { useInView } from "react-intersection-observer";
-type Props = {};
 
-export default function Experience({}: Props) {
-  const { ref } = useSectionInView("Experience");
+type ExperienceItemType = {
+  title: string;
+  location: string;
+  description: string;
+  icon: React.ReactElement;
+  date: string;
+};
+
+type ExperienceItemProps = {
+  item: ExperienceItemType;
+};
+
+const ExperienceItem = ({ item }: ExperienceItemProps) => {
   const { theme } = useTheme();
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+  });
+
+  return (
+    <div ref={ref} className="vertical-timeline-element">
+      <VerticalTimelineElement
+        visible={inView}
+        contentStyle={{
+          background:
+            theme === "light" ? "#f3f4f6" : "rgba(255, 255, 255, 0.05)",
+          boxShadow: "none",
+          border: "1px solid rgba(0, 0, 0, 0.05)",
+          textAlign: "left",
+          padding: "1.3rem 2rem",
+        }}
+        contentArrowStyle={{
+          borderRight:
+            theme === "light"
+              ? "0.4rem solid #9ca3af"
+              : "0.4rem solid rgba(255, 255, 255, 0.5)",
+        }}
+        date={item.date}
+        icon={item.icon}
+        iconStyle={{
+          background:
+            theme === "light" ? "white" : "rgba(255, 255, 255, 0.15)",
+          fontSize: "1.5rem",
+        }}
+      >
+        <h3 className="font-semibold capitalize">{item.title}</h3>
+        <p className="font-normal !mt-0">{item.location}</p>
+        <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">
+          {item.description}
+        </p>
+      </VerticalTimelineElement>
+    </div>
+  );
+};
+
+export default function Experience() {
+  const { ref } = useSectionInView("Experience", 0.5);
 
   return (
     <section ref={ref} id="experience" className="scroll-mt-28 mb-28 sm:mb-40">
       <SectionHeader>My Experience</SectionHeader>
       <VerticalTimeline lineColor="">
-        {experiencesData.map((item, index) => {
-          const { ref, inView } = useInView({
-            triggerOnce: true,
-          });
-          return (
-            <div key={index} ref={ref} className="vertical-timeline-element">
-              <VerticalTimelineElement
-                visible={inView}
-                contentStyle={{
-                  background:
-                    theme === "light" ? "#f3f4f6" : "rgba(255, 255, 255, 0.05)",
-                  boxShadow: "none",
-                  border: "1px solid rgba(0, 0, 0, 0.05)",
-                  textAlign: "left",
-                  padding: "1.3rem 2rem",
-                }}
-                contentArrowStyle={{
-                  borderRight:
-                    theme === "light"
-                      ? "0.4rem solid #9ca3af"
-                      : "0.4rem solid rgba(255, 255, 255, 0.5)",
-                }}
-                date={item.date}
-                icon={item.icon}
-                iconStyle={{
-                  background:
-                    theme === "light" ? "white" : "rgba(255, 255, 255, 0.15)",
-                  fontSize: "1.5rem",
-                }}
-              >
-                <h3 className="font-semibold capitalize">{item.title}</h3>
-                <p className="font-normal !mt-0">{item.location}</p>
-                <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">
-                  {item.description}
-                </p>
-              </VerticalTimelineElement>
-            </div>
-          );
-        })}
+        {experiencesData.map((item, index) => (
+          <ExperienceItem key={index} item={item} />
+        ))}
       </VerticalTimeline>
     </section>
   );
